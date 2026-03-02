583150b2ed34d6b08c89eb89e3eec9384c058d31
LouisAE <88891403+louisae@users.noreply.github.com>
2026-03-02 15:10:23 +0800

    Modify generic example to test opus

--- examples/generic/main.c
+++ examples/generic/main.c
@@ -1,3 +1,5 @@
+#include <opus/opus.h>
+
 #include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
@@ -85,26 +87,6 @@ static uint64_t get_timestamp() {
   return tv.tv_sec * 1000 + tv.tv_usec / 1000;
 }
 
-void peer_log(char* level_tag, const char* file_name, int line_number, const char* fmt, ...)
-{
-  char* pos = file_name, *ptr = NULL;
-  va_list args;
-
-  for (ptr = file_name; *ptr != '\0'; ptr++)
-  {
-    if (*ptr == '/')
-    {
-      pos = ptr + 1;
-    }
-  }
-
-  printf("[%s] %s:%s", level_tag, pos, line_number);
-  
-  va_start(args, fmt);
-  vprintf(fmt, args);
-  va_end(args);
-}
-
 void print_usage(const char* prog_name) {
   printf("Usage: %s -u <url> [-t <token>]\n", prog_name);
 }
@@ -130,12 +112,37 @@ void parse_arguments(int argc, char* argv[], const char** url, const char** toke
   }
 }
 
+static int16_t alaw2linear(uint8_t a_val) {
+  int t;
+  int seg;
+
+  a_val ^= 0x55;
+  t = (a_val & 0x0f) << 4;
+  seg = ((int)(a_val & 0x70)) >> 4;
+  switch (seg) {
+    case 0:
+      t += 8;
+      break;
+    case 1:
+      t += 0x108;
+      break;
+    default:
+      t += 0x108;
+      t <<= seg - 1;
+  }
+  return ((a_val & 0x80) ? t : -t);
+}
+
 int main(int argc, char* argv[]) {
   uint64_t curr_time, video_time, audio_time;
   uint8_t* buf = NULL;
   const char* url = NULL;
   const char* token = NULL;
   int size;
+  int opus_err = 0;
+  OpusEncoder* enc = NULL;
+  unsigned char opus_buf[1024]; // Increased buffer size and changed to unsigned char
+  opus_int32 opus_len = 0;
 
   pthread_t peer_singaling_thread;
   pthread_t peer_connection_thread;
@@ -150,12 +157,28 @@ int main(int argc, char* argv[]) {
       },
       .datachannel = DATA_CHANNEL_STRING,
       .video_codec = CODEC_H264,
-      .audio_codec = CODEC_PCMA};
+      .audio_codec = CODEC_OPUS};
 
   printf("=========== Parsed Arguments ===========\n");
   printf(" %-5s : %s\n", "URL", url);
   printf(" %-5s : %s\n", "Token", token ? token : "");
   printf("========================================\n");
+
+  enc = opus_encoder_create(8000, 1, OPUS_APPLICATION_AUDIO, &opus_err);
+  if (opus_err != OPUS_OK)
+  {
+    printf("Failed to create opus encoder: %s\n", opus_strerror(opus_err));
+    return -1;
+  }
+
+  opus_encoder_ctl(enc, OPUS_SET_VBR(0));//0:CBR, 1:VBR
+  opus_encoder_ctl(enc, OPUS_SET_VBR_CONSTRAINT(1));
+  opus_encoder_ctl(enc, OPUS_SET_BITRATE(48000));
+  opus_encoder_ctl(enc, OPUS_SET_COMPLEXITY(8));//8    0~10
+  opus_encoder_ctl(enc, OPUS_SET_SIGNAL(OPUS_SIGNAL_VOICE));
+  opus_encoder_ctl(enc, OPUS_SET_LSB_DEPTH(16));//每个采样16个bit，2个byte
+  opus_encoder_ctl(enc, OPUS_SET_DTX(0));
+  opus_encoder_ctl(enc, OPUS_SET_INBAND_FEC(0));
 
   peer_init();
   g_pc = peer_connection_create(&config);
@@ -172,11 +195,11 @@ int main(int argc, char* argv[]) {
   while (!g_interrupted) {
     if (g_state == PEER_CONNECTION_COMPLETED) {
       curr_time = get_timestamp();
-
       // FPS 25
       if (curr_time - video_time > 40) {
         video_time = curr_time;
         if ((buf = reader_get_video_frame(&size)) != NULL) {
+
           peer_connection_send_video(g_pc, buf, size);
           // need to free the buffer
           free(buf);
@@ -186,7 +209,21 @@ int main(int argc, char* argv[]) {
 
       if (curr_time - audio_time > 20) {
         if ((buf = reader_get_audio_frame(&size)) != NULL) {
-          peer_connection_send_audio(g_pc, buf, size);
+          int16_t pcm_buf[320]; // 160 samples * sizeof(int16) is safe, make it larger
+
+          for(int i=0; i < size; i++) {
+             pcm_buf[i] = alaw2linear(buf[i]);
+          }
+
+          // 8000Hz * 20ms / 1000 = 160 samples
+          opus_len = opus_encode(enc, pcm_buf, size, opus_buf, sizeof(opus_buf));
+          if (opus_len < 0)
+          {
+            printf("opus encode failed: %s\n", opus_strerror(opus_len));
+          } else {
+            printf("size:%d\n", opus_len);
+            peer_connection_send_audio(g_pc, opus_buf, opus_len);
+          }
           buf = NULL;
         }
         audio_time = curr_time;
